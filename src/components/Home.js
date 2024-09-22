a255fb3fc9d31e35ca56f9dcc421f302aa7e1a69
niyas <nzmniyascse@gmail.com>
2024-09-23 05:23:39 +0530

    Added Buy button logic

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,65 @@ import { useEffect, useState } from 'react';
 
 import close from '../assets/close.svg';
 
-const Home = ({ home, provider, escrow, togglePop }) => {
+const Home = ({ home, provider, account, escrow, togglePop }) => {
+    
+    const [hasBought, setHasBought] = useState(false)
+    const [hasLended, setHasLended] = useState(false)
+    const [hasInspected, setHasInspected] = useState(false)
+    const [hasSold, setHasSold] = useState(false)
+
+    const [buyer, setBuyer] = useState(null)
+    const [seller, setSeller] = useState(null)
+    const [inspector, setInspector] = useState(null)
+    const [lender, setLender] = useState(null)
+
+    const[owner, setOwner] = useState(null)
+
+    console.log(account);
+
+    const fetchDetails = async () => {
+        // -- Buyer
+        const buyer = await escrow.buyer(home.id)
+        setBuyer(buyer)
+
+        const hasBought = await escrow.approval(home.id, buyer)
+        setHasBought(hasBought)
+
+        // -- sellr
+        const seller = await escrow.seller()
+        setSeller(seller)
+
+        const hasSold = await escrow.approval(home.id, seller)
+        setHasSold(hasSold)
+        
+
+        // -- lender
+        const lender = await escrow.lender()
+        setLender(lender)
+
+        const hasLended = await escrow.approval(home.id, lender)
+        setHasLended(hasLended)
+
+        // -- inspector
+        const inspector = await escrow.inspector()
+        setInspector(inspector)
+
+        const hasInspected = await escrow.inspectionPassed(home.id)
+        setHasInspected(hasInspected)
+    }
+
+    const fetchOwner = async () => {
+        
+        if(await escrow.isListed(home.id)) return
+
+        const owner = await escrow.buyer(home.id)
+        setOwner(owner)
+    }
+
+    useEffect(() => {
+        fetchDetails()
+        fetchOwner()
+    }, [hasSold])
 
     return (
         <div className="home">
@@ -23,14 +81,41 @@ const Home = ({ home, provider, escrow, togglePop }) => {
                         {home.address}
                     </p>
                     <h2>{home.attributes[0].value} ETH</h2>
+
+                    {owner ? (
+                        <div className='home__owned'>
+                            Owned by {owner.slice(0,6) + '...' + owner.slice(38,42)}
+                        </div>    
+                    ) : (
+                        <div>
+                            {(account === inspector) ? (
+                                <button className='home__buy'>
+                                    Approved Inspection
+                                </button>
+
+                            ) : (account === lender) ? (
+                                <button className='home__buy'>
+                                    Approve & Lend
+                                </button>
+                            ) : (account === seller) ? (
+                                <button className='home__buy'>
+                                    Approve & Sell
+                                </button>
+                            ) : (
+                                <button className='home__buy' >
+                                Buy
+                                </button>
+                            )}
+
+                            <button className='home__contact'>
+                            Contact Agent
+                            </button>
+                        </div>
+                    )}
                     <div>
-                        <button className='home__buy' >
-                            Buy
-                        </button>
+                       
                     </div>
-                    <button className='home__contact'>
-                        Contact Agent
-                    </button>
+                 
                     <hr />
                     <h2>Overview</h2>
                     <p>
