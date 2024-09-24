04fdc5449912a64227f676b9eab8f2ee0dbaa95a
niyas <nzmniyascse@gmail.com>
2024-09-25 05:19:18 +0530

    Added Buy, Sell, Inspect and Lend Handler

--- src/components/Home.js
+++ src/components/Home.js
@@ -58,6 +58,59 @@ const Home = ({ home, provider, account, escrow, togglePop }) => {
         setOwner(owner)
     }
 
+    const buyHandler = async () => {
+        const escrowAmount =  await escrow.escrowAmount(home.id)
+        const signer =  await provider.getSigner()
+
+        // Buyer deposit earnest
+        let transaction = await escrow.connect(signer).depositEarnest(home.id, {value: escrowAmount})
+        await transaction.wait()
+
+        // Buyer approves....
+        transaction = await escrow.connect(signer).approveSale(home.id)
+        await transaction.wait()
+
+        setHasBought(true)
+    }
+
+    const inspectHandler = async () => {
+        const signer = await provider.getSigner()
+
+        // Inspector update status
+        const transaction = await escrow.connect(signer).updateInspectionStatus(home.id, true)
+        await transaction.wait()
+
+        setHasInspected(true)
+    }
+
+    const lendHandler = async () => {
+        const signer = await provider.getSigner()
+
+        // Lender Approves....
+        const transaction = await escrow.connect(signer).approveSale(home.id)
+        await transaction.wait()
+
+        // Lender sends funds to contract..
+        const lendAmount = (await escrow.purchasePrice(home.id) -  await escrow.escrowAmount(home.id))
+        await signer.sendTransaction({to: escrow.address, value: lendAmount.toString(), gaslimit: 6000 })
+
+        setHasLended(true)
+    }
+
+    const sellHandler = async () => {
+        const signer = await provider.getSigner()
+
+        //Seller Aproves...
+        const transaction = await escrow.connect(signer).approveSale(home.id)
+        await transaction.wait()
+
+        // Seller Finalize
+        transaction = await escrow.connect(signer).finalizeSale(home.id)
+        await transaction.wait()
+
+        setHasSold(true)
+    }
+
     useEffect(() => {
         fetchDetails()
         fetchOwner()
@@ -89,20 +142,20 @@ const Home = ({ home, provider, account, escrow, togglePop }) => {
                     ) : (
                         <div>
                             {(account === inspector) ? (
-                                <button className='home__buy'>
+                                <button className='home__buy' onClick={inspectHandler} disabled={hasInspected}>
                                     Approved Inspection
                                 </button>
 
                             ) : (account === lender) ? (
-                                <button className='home__buy'>
+                                <button className='home__buy' onClick={lendHandler} disabled={hasLended}>
                                     Approve & Lend
                                 </button>
                             ) : (account === seller) ? (
-                                <button className='home__buy'>
+                                <button className='home__buy' onClick={sellHandler} disabled={hasSold}>
                                     Approve & Sell
                                 </button>
                             ) : (
-                                <button className='home__buy' >
+                                <button className='home__buy' onClick={buyHandler} disabled={hasBought}>
                                 Buy
                                 </button>
                             )}
